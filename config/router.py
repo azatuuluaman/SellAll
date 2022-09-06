42dd6c8392a255e4ae23c41e47878d672178fb36
KU-Baha <kulbarakovich@gmail.com>
2022-09-06 14:36:51 +0600

    Router refactoring

--- config/router.py
+++ config/router.py
@@ -2,30 +2,16 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from advertisement.views import (
-    AdvertisementAPIView,
-    CategoryAPIView,
-    ViewStatisticAPIView,
-    CityAPIView,
-    ChildCategoryAPIView,
-    AdsSubscriberAPIView,
-    AdsCommentCreateView,
-    AdsCommentRUDView, CategoryRetrieveAPIView
-)
-from siteapp.views import (
-    SiteAPIView,
-    SocialMediaAPIView,
-    FeedBackAPIView,
-    HelpAPIView, HelpCategoryAPIView
-)
+from advertisement.views import AdvertisementAPIView
 from user.views import RegisterUserView, ActivationView
 
 router = routers.DefaultRouter()
-
 router.register('advertisement', AdvertisementAPIView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('siteapp.urls')),
+    path('', include('advertisement.urls')),
 ]
 
 urlpatterns_auth = [
@@ -33,25 +19,4 @@ urlpatterns_auth = [
     path('activation/<str:code>/', ActivationView.as_view(), name='activate'),
 ]
 
-urlpatterns_ads = [
-    path('categories/', CategoryAPIView.as_view(), name='categories'),
-    path('category/<int:pk>/', CategoryRetrieveAPIView.as_view(), name='category'),
-    path('child-categories/', ChildCategoryAPIView.as_view(), name='child_categories'),
-    path('cities/', CityAPIView.as_view(), name='cities'),
-    path('statistic/<int:pk>/', ViewStatisticAPIView.as_view(), name='statistic'),
-    path('subscribers/', AdsSubscriberAPIView.as_view(), name='subscribers'),
-    path('comment/', AdsCommentCreateView.as_view(), name='comment_create'),
-    path('comment/<int:pk>/', AdsCommentRUDView.as_view(), name='comment_rud')
-]
-
-urlpatterns_siteapp = [
-    path('site/', SiteAPIView.as_view(), name='site'),
-    path('social-media/', SocialMediaAPIView.as_view(), name='social_media'),
-    path('feedback/', FeedBackAPIView.as_view(), name='feedback'),
-    path('help-category/', HelpCategoryAPIView.as_view(), name='help_category'),
-    path('help/<int:pk>/', HelpAPIView.as_view(), name='help'),
-]
-
 urlpatterns += urlpatterns_auth
-urlpatterns += urlpatterns_ads
-urlpatterns += urlpatterns_siteapp
