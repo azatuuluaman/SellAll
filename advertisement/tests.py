22812b2b709941b2fb0dd7744289ae124dc51505
KU-Baha <kulbarakovich@gmail.com>
2022-09-12 10:04:14 +0600

    Some tests

--- advertisement/tests.py
+++ advertisement/tests.py
@@ -3,6 +3,8 @@ from rest_framework import status
 
 from .models import City
 
+API_URL = '/api/v1/'
+
 
 class CityTestCase(APITestCase):
     city_list = ['Bishkek', 'Osh', 'Jalal-Abad ', 'Karakol ', 'Tokmok', 'Kara-Balta']
@@ -11,10 +13,9 @@ class CityTestCase(APITestCase):
     def test_create_and_list(self):
         for city in self.city_list:
             data = {'name': city}
-            response = self.client.post('/api/city/', data)
+            response = self.client.post(f'{API_URL}/city/', data)
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        response = self.client.get('/api/city/')
+        response = self.client.get(f'{API_URL}/city/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(self.city_list), self.model.objects.count())
-
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from advertisement.utils import Redis
+
+User = get_user_model()
+
+API_URL = '/api/v1/'
+
+
+class UserTestCase(APITestCase):
+    model = User
+    email = 'admin@gmail.com'
+    password = 'admin12345'
+    activate_code = 1234
+
+    def test_register(self):
+        data = {
+            'email': self.email,
+            'first_name': 'Admin',
+            'last_name': 'Gmail',
+            'password': self.password,
+            'password_confirm': self.password
+        }
+        response = self.client.post(f'{API_URL}/register/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        key = f'activate_code_{self.activate_code}'
+        user = User.objects.get(email=self.email)
+
+        redis = Redis()
+        redis.conn.set(key, user.pk)
+
+        data = {
+            'activate_code': self.activate_code
+        }
+
+        response = self.client.post(f'{API_URL}/activation/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(user.is_active, True)
+        self.assertEqual(redis.conn.get(key), None)
