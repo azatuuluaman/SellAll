956d4267b63c8015623af058aeca37567ebc4d95
KU-Baha <kulbarakovich@gmail.com>
2022-09-12 13:22:45 +0600

    help dump file

--- advertisement/management/commands/dump_categories.py
+++ advertisement/management/commands/dump_categories.py
@@ -1,6 +1,4 @@
 from django.core.management.base import BaseCommand
-from django.core.files import File
-from django.conf import settings
 
 from advertisement.models import Category, ChildCategory
 
