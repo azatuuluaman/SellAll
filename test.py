7bfd8a1b0c2055b3f4e6c9e8cdb9cee326e18d49
KU-Baha <kulbarakovich@gmail.com>
2022-09-17 14:30:03 +0600

    docekr compose

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-a = int(input())
-b = int(input())
-m = int(input())
+# a = int(input())
+# b = int(input())
+# m = int(input())
 
-print(pow(a, b))
-print(pow(a, b, m))
+# print(pow(a, b))
+# print(pow(a, b, m))
