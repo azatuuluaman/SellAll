64663e3e2d98fb077bb1a1188fa0ac883553b702
KU-Baha <kulbarakovich@gmail.com>
2022-09-17 16:59:49 +0600

    Delete default validate

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
 from .swagger_config import urlpatterns as swg
 
@@ -29,7 +29,6 @@ urlpatterns = [
     path(f'{API_HEAD_URL}/social-auth/', include('social_auth.urls'), name='social'),
     path(f'{API_HEAD_URL}/token/', TokenObtainPairView.as_view(), name='login'),
     path(f'{API_HEAD_URL}/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
-    path(f'{API_HEAD_URL}/token/verify/', TokenVerifyView.as_view(), name='verify'),
     path(f'{API_HEAD_URL}/logout/', TokenBlacklistView.as_view(), name='logout'),
 
     path(f'{API_HEAD_URL}/user/', include('user.urls')),
